f0ad9a2792676059efde5c48230f818c36d880cd
Humayan Al Rosid <sony.leo.sony@gmail.com>
2023-04-12 14:20:44 +0530

    add static

--- blog/settings.py
+++ blog/settings.py
@@ -114,3 +114,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 MEDIA_URL = '/media/'  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "app/static"]
--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
